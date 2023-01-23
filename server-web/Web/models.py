c6da35795200f2049433c0baf81e1fa270968a1d
stormlighter72 <cocorich72@gmail.com>
2023-01-23 14:57:20 +0000

    Premier Bridge fonctionnel

--- server-web/Web/models.py
+++ server-web/Web/models.py
@@ -5,3 +5,4 @@ from django.db import models
 class Entrepot(models.Model):
     name = models.CharField(max_length=30)
     packages = models.ManyToManyField('Package', through = 'PackageEntrepot')
+    
