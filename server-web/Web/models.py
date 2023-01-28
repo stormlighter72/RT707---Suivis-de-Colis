5d3a4cdb8e16271c41edbf749c44743fa7457f3a
stormlighter72 <cocorich72@gmail.com>
2023-01-28 15:09:17 +0000

    Dernier Commit

--- server-web/Web/models.py
+++ server-web/Web/models.py
@@ -5,4 +5,14 @@ from django.db import models
 # class Entrepot(models.Model):
 #     name = models.CharField(max_length=30)
 #     packages = models.ManyToManyField('Package', through = 'PackageEntrepot')
+
+# class Clients(models.Model):
+#     nom = models.CharField(max_length=30)
+#     prenom = models.CharField(max_length=30)
+#     num_tel = models.IntegerField(max_length=10)
+#     addr_mail = models.CharField(max_length=30)
+#     addr_postale = models.CharField(max_length=30)
+#     code_postal= models.IntegerField(max_length=5)
+#     ville= models.CharField(max_length=10)
+    
     
